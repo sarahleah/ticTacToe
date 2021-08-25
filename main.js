0a62b90e0ca44c601673f6f42562c860556b39f3
sarahandreas <sarah.andreas@gmail.com>
2021-08-25 14:52:24 +1000

    game base functionality

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 let gameBoard = document.querySelector('.game-board')
+let columns = document.querySelectorAll('.column')
 
 gameBoard.addEventListener('click', handleTurn)
 
@@ -7,36 +8,70 @@ let counter = 0
 function handleTurn(event) {
     let selectedTile = event.target
     printLetter(selectedTile)
-    let tileCoords = getTileCoords(selectedTile)
-    checkForWin(tileCoords)
+    let result = checkForWin(makeGameMatrix())
+    if (result) {
+        console.log('win')
+    } else {
+        console.log('nothing yet')
+    }
 }
 
 function printLetter(selectedTile) {
-    if (selectedTile.textContent === '') {
+    if (selectedTile.textContent === '#') {
         if (counter % 2 === 0) {
             selectedTile.textContent = 'X'
         } else {
             selectedTile.textContent = 'O'
         }
+        selectedTile.style.color = 'black'
         counter++
     }
 }
 
-function getTileCoords(selectedTile) {
-    let tileCoords = selectedTile.classList.value
-    tileCoords = tileCoords.split('')
-                .filter(element => Number(element))
-                .map(element => Number(element))
-    while (tileCoords.length < 2) {
-        tileCoords.unshift(0)
+function makeGameMatrix() {
+    let matrix = []
+    let whitespaceRX = /\s/g
+    for (let div of columns) {
+        let columnContent
+        columnContent = div.textContent.replace(whitespaceRX, '').split('')
+        matrix.push(columnContent)
     }
-    return tileCoords
+    return matrix
 }
 
-function checkForWin(tileCoords) {
-    let mainTileX = tileCoords[0]
-    let mainTileY = tileCoords[1]
-    function diagCheck(mainTileX, mainTileY) {
+function checkForWin(matrix) {
+    // returns true for win
+    // returns false for loss
 
+    let x0 = matrix[0]
+    let x1 = matrix[1]
+    let x2 = matrix[2]
+
+    console.log(x0, x1, x2)
+
+    const testX = (element) => {
+         return element === 'X'
     }
+    const testO = element => {
+        return element === 'O'
+    }
+    for (let column of matrix) {
+        if (column.every(testX) || column.every(testO)) {
+            return true
+        }
+    }
+    for (let y in matrix) {
+        y = Number(y)
+        console.log(typeof x)
+        if (x0[y] === x1[y] && x1[y] === x2[y] && x0[y] !== '#') {
+            return true
+        }
+    }
+    if (x0[0] === x1[1] && x1[1] === x2[2] && x0[0] !== '#') {
+        return true
+    }
+    if (x0[2] === x1[1] && x1[1] === x2[0] && x0[2] !== '#') {
+        return true
+    }
+    return false
 }
